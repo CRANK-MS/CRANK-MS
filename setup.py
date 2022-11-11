a325b0261616e1154b7b3ffb13391fa2b37dd772
Chonghua Xue <cxue2@bu.edu>
2022-11-11 15:21:32 -0500

    #1

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='crank-ms',
+    version='0.1',
+    author='Chonghua Xue',
+    author_email='cxue2@bu.edu',
+    # url='https://github.com/vkola-lab/python_speech_features_cuda',
+    # description='Re-implementation of Python Speech Features Extraction on CUDA.',
+    packages=setuptools.find_packages(),
+    # classifiers=[
+    #     'Environment :: GPU :: NVIDIA CUDA',
+    #     'Operating System :: OS Independent',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Programming Language :: Python :: 3'
+    # ]
+)
