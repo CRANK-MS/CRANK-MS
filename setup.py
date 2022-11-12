7eaef072d4f0fac46e9741793629fe12cd87d469
Chonghua Xue <cxue2@bu.edu>
2022-11-12 17:25:40 -0500

    #7

--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ setuptools.setup(
     # description = 'Re-implementation of Python Speech Features Extraction on CUDA.',
     packages = setuptools.find_packages(),
     python_requires = '>=3.7',
-    # classifiers=[
-    #     'Environment :: GPU :: NVIDIA CUDA',
-    #     'Operating System :: OS Independent',
-    #     'License :: OSI Approved :: MIT License',
-    #     'Programming Language :: Python :: 3'
-    # ]
+    classifiers=[
+        'Environment :: GPU :: NVIDIA CUDA',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'numpy',
+        'torch',
+        'tqdm',
+        'sklearn',
+    ],
 )
